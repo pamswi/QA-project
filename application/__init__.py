f9e471eff8ad429c38cfa849da6085caea80bcce
Pam Swi <pamswiderska@gmail.com>
2023-08-15 12:12:28 +0100

    PS: db config for jenkins

--- application/__init__.py
+++ application/__init__.py
@@ -4,13 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+db = SQLAlchemy
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
+db.init_app(app)
 
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-db = SQLAlchemy(app)
 import application.routes
